abd1797b465cffbe88c15d4d1e7d0d65078dd549
leandrobusa <leandrobusa658@gmail.com>
2026-07-07 21:19:59 -0300

    feat: consolida banco e prepara backend do atendelab

--- routes.php
+++ routes.php
@@ -2,6 +2,9 @@
 
 require_once __DIR__ . '/app/Controllers/AuthController.php';
 require_once __DIR__ . '/app/Controllers/UsuariosController.php';
+require_once __DIR__ . '/app/Controllers/PessoasController.php';
+require_once __DIR__ . '/app/Controllers/TiposAtendimentosController.php';
+require_once __DIR__ . '/app/Controllers/AtendimentosController.php';
 require_once __DIR__ . '/app/Middleware/auth.php';
 
 $controller = $_GET['controller'] ?? 'auth';
@@ -62,6 +65,95 @@ switch ($controller) {
             default:
                 http_response_code(404);
                 echo 'Acao de usuarios nao encontrada.';
+        }
+        break;
+
+    case 'pessoas':
+        exigirAutenticacao();
+        $pessoasController = new PessoasController();
+
+        switch ($action) {
+            case 'listar':
+                $pessoasController->listar();
+                break;
+
+            case 'buscar':
+                $pessoasController->buscar();
+                break;
+
+            case 'criar':
+                $pessoasController->criar();
+                break;
+
+            case 'atualizar':
+                $pessoasController->atualizar();
+                break;
+
+            case 'inativar':
+                $pessoasController->inativar();
+                break;
+
+            default:
+                http_response_code(404);
+                echo 'Acao de pessoas nao encontrada.';
+        }
+        break;
+
+    case 'tipos':
+        exigirAutenticacao();
+        $tiposController = new TiposAtendimentosController();
+
+        switch ($action) {
+            case 'listar':
+                $tiposController->listar();
+                break;
+
+            case 'buscar':
+                $tiposController->buscar();
+                break;
+
+            case 'criar':
+                $tiposController->criar();
+                break;
+
+            case 'atualizar':
+                $tiposController->atualizar();
+                break;
+
+            case 'inativar':
+                $tiposController->inativar();
+                break;
+
+            default:
+                http_response_code(404);
+                echo 'Acao de tipos nao encontrada.';
+        }
+        break;
+
+    case 'atendimentos':
+        exigirAutenticacao();
+        $atendimentosController = new AtendimentosController();
+
+        switch ($action) {
+            case 'listar':
+                $atendimentosController->listar();
+                break;
+
+            case 'buscar':
+                $atendimentosController->buscar();
+                break;
+
+            case 'criar':
+                $atendimentosController->criar();
+                break;
+
+            case 'alterarStatus':
+                $atendimentosController->alterarStatus();
+                break;
+
+            default:
+                http_response_code(404);
+                echo 'Acao de atendimentos nao encontrada.';
         }
         break;
 
